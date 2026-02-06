79e86062269acb02afafdd482b0519640fd12955
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-02-06 17:51:18 +0530

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,22 @@
+// Projects data - REAL LIVE LINKS
+const projects = [
+    {
+        title: "Personal Portfolio",
+        author: "Tharun Kumar", 
+        description: "Designed and developed a responsive personal portfolio website to showcase my skills, projects, internships, and achievements. Built using HTML, Tailwind CSS, and JavaScript, with smooth navigation and a clean UI.",
+        tech: ["HTML", "TailwindCSS", "JavaScript"],
+        liveUrl: "https://tarun25-dev.github.io/portfolio/", 
+        repoUrl: "https://github.com/Tarun25-dev/portfolio/"
+    },
+    {
+        title: "Smart Sorting", 
+        author: "Tharun Kumar",
+        description: "Smart Sorting is an AI-powered system designed to detect and classify rotten fruits and vegetables using transfer learning with the VGG16 model.",
+        tech: ["TensorFlow/Keras", "Python", "Flask","OpenCV"],
+        liveUrl: "https://github.com/Tarun25-dev/Smart-sorting/",
+        repoUrl: "https://github.com/Tarun25-dev/Smart-sorting/"
+    },
+
+];
+
+
